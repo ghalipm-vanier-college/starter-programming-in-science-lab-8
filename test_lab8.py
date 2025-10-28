3c9f0457a52ef8a44ce717aaace1991a2c3d49a9
Ghalip Memet <45562923+ghalipm@users.noreply.github.com>
2025-10-28 08:45:18 -0400

    Replace magic square tests with dot product tests
    test file update following Lab file update

--- test_lab8.py
+++ test_lab8.py
@@ -1,21 +1,19 @@
+# DO NOT CHANGE THIS FILE, OR ZERO MARK WILL BE ASSIGNED.
 import unittest
-from Lab8 import is_magic_square, generate_magic_square
+from Lab8 import dot_product, is_orthogonal
 
-# Test case for is_magic_square() function
-def test_is_magic_square():
-    assert is_magic_square([[4, 9, 2], [3, 5, 7], [8, 1, 6]]) == True
-    assert is_magic_square([[4, 9, 2], [8, 1, 6], [3, 5, 7]]) == False
 
-# Test case for generate_magic_square() function
-def test_generate_magic_square():
-    assert generate_magic_square(3) == [[4, 9, 2], [3, 5, 7], [8, 1, 6]]
-    assert generate_magic_square(5) == [
-        [11, 18, 25, 2, 9],
-        [10, 12, 19, 21, 3],
-        [4, 6, 13, 20, 22],
-        [23, 5, 7, 14, 16],
-        [17, 24, 1, 8, 15]
-    ]
+# Test for dot_product()
+def test_dot_product():
+    assert dot_product([1, 2, 3], [4, 5, 6]) == 32
+    assert dot_product([2, 0, -1], [3, 4, 5]) == 1
+    assert dot_product([0, 0, 0], [4, 5, 6]) == 0
+    assert dot_product([5], [7]) == 35
 
-if __name__ == '__main__':
-    unittest.main()
+
+# Test for is_orthogonal()
+def test_is_orthogonal():
+    assert is_orthogonal([1, 2], [-2, 1]) == True     # perpendicular
+    assert is_orthogonal([2, 3], [4, 5]) == False     # not perpendicular
+    assert is_orthogonal([0, 0], [1, 2]) == False     # zero vector → not valid orthogonal
+    assert is_orthogonal([3, -3], [3, 3])_
